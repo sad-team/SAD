33fd19c1182467bae944e78ba8120719b4f80851
HondouEisuke00 <33284693+HondouEisuke00@users.noreply.github.com>
2019-04-11 20:16:14 +0800

    Post测试
    使用在线测试如果需要中文，要设置为UTF-8和不解压

--- background/src/main/java/SAD/Try/tryServlet.java
+++ background/src/main/java/SAD/Try/tryServlet.java
@@ -6,19 +6,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
 
 public class tryServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setCharacterEncoding("GBK");
-        resp.setContentType("application/xml; charset=GBK");
-        BufferedReader reader=req.getReader();
-        StringBuilder rawstring=new StringBuilder();
-        String inputline;
-        while((inputline=reader.readLine())!=null){
-            rawstring.append(inputline);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp){
+        try {
+            req.setCharacterEncoding("GBK");
+            resp.setCharacterEncoding("GBK");
+            resp.setContentType("application/xml; charset=GBK");
+            BufferedReader reader = req.getReader();
+            StringBuilder rawstring = new StringBuilder();
+            String inputline;
+            while ((inputline = reader.readLine()) != null) {
+                rawstring.append(inputline);
+            }
+            Map<String, String> post_key_value = JSON.parseObject(rawstring.toString(), Map.class);
+            PrintWriter writer = resp.getWriter();
+            writer.print(post_key_value);
+        }catch(Exception e){
+            return;
         }
-        Map<String,String> post_key_value= JSON.parseObject(rawstring.toString(),Map.class);
     }
 }
