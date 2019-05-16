d64a429f32ccd071738c93121dcd06b762679149
HondouEisuke00 <33284693+HondouEisuke00@users.noreply.github.com>
2019-05-16 12:06:05 +0800

    no message

--- SAD/src/main/java/SAD/Test/DatabaseTest.java
+++ SAD/src/main/java/SAD/Test/DatabaseTest.java
@@ -6,6 +6,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DatabaseTest {
     private static DataOperation dataoperator;
     static{
@@ -30,5 +33,21 @@ public class DatabaseTest {
     @Test
     public void getUserOrderTest(){
         System.out.println(dataoperator.getUserOrder(1));
+    }
+    @Test
+    public void followUserTest(){
+        System.out.println(dataoperator.followUser(6,7));
+    }
+    @Test
+    public void unFollowUserTest(){
+        System.out.println(dataoperator.unFollowUser(6,7));
+    }
+    @Test
+    public void sendMessageTest(){
+        System.out.println(dataoperator.sendMessage(6,7,"this is a test",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())));
+    }
+    @Test
+    public void getMessageTest(){
+        System.out.println(dataoperator.getMessage(7));
     }
 }
