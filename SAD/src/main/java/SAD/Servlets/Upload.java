71ed39386f3245fb060c8a4b20e7f2b0824e3934
ZombieSue <804780457@qq.com>
2019-05-19 00:23:12 +0800

    d

--- SAD/src/main/java/SAD/Servlets/Upload.java
+++
@@ -1,9 +0,0 @@
-package SAD.Servlets;
-
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.http.HttpServlet;
-
-@MultipartConfig(maxFileSize = 1024*1024*512, maxRequestSize = 1024*1024*1024)
-public class Upload extends HttpServlet {
-
-}
